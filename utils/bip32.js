19a69d122183242528da76cfe7471320791df3ed
jcgloria <96juancg@gmail.com>
2022-04-24 11:05:57 -0500

    bip32 public key implementation. Still some bugs to fix

--- utils/bip32.js
+++ utils/bip32.js
@@ -1,6 +1,8 @@
-import { createHmac, createECDH, createHash } from 'crypto'
+import { createHmac, createHash } from 'crypto'
 import pkg from 'bs58';
 const { encode }  = pkg;
+import { generatePublicKeyFromPrivateKeyData ,uint256} from '@enumatech/secp256k1-js';
+import { convertBufferToBinaryString, findParity } from './general.js'
 
 function generateMasterKey(seed){
     const hmac = createHmac('sha512', 'Bitcoin seed')
@@ -14,21 +16,32 @@ function generateMasterKey(seed){
     const index = 0
     const child = Buffer.alloc(4)
     child.writeUInt32BE(index)
-    const k_priv = createECDH('secp256k1')
-    k_priv.setPrivateKey(Il)
-    const K_priv = k_priv.getPublicKey() 
-    const data_priv = Buffer.concat([Buffer.alloc(1),k_priv.getPrivateKey()])
-
+    const privateKey = uint256(Il, 16)
+    const publicKey = generatePublicKeyFromPrivateKeyData(privateKey)
+    const yString = convertBufferToBinaryString(Buffer.from(publicKey.y, 'hex'))
+    let data_pub;
+    if(findParity(yString)){
+        data_pub = Buffer.concat([Buffer.from('02', 'hex'), Buffer.from(publicKey.x, 'hex')])
+    }else{
+        data_pub = Buffer.concat([Buffer.from('03', 'hex'), Buffer.from(publicKey.x, 'hex')])
+    }
+    const data_priv = Buffer.concat([Buffer.alloc(1),Il])
     const raw_priv = Buffer.concat([version_priv,depth,fp,child,Ir,data_priv])
-    //const raw_pub = xpub + depth + fp + child + Ir + data_pub
-
+    const raw_pub = Buffer.concat([version_pub, depth,fp,child,Ir,data_pub])
     const priv_hash = createHash('sha256').update(raw_priv).digest()
     const priv_hash2 = createHash('sha256').update(priv_hash).digest()
+    const pub_hash = createHash('sha256').update(raw_pub).digest()
+    const pub_hash2 = createHash('sha256').update(pub_hash).digest()
     //Concat checksum (double sha256) and base58 encode
-    const xpriv = encode(Buffer.concat([raw_priv,priv_hash2.slice(0,4)])) 
+    const xpriv = encode(Buffer.concat([raw_priv,priv_hash2.slice(0,4)]))
+    const xpub = encode(Buffer.concat([raw_pub, pub_hash2.slice(0,4)])) 
+    console.log('XPUB')
+    console.log(xpub)
+    console.log('xpub661MyMwAqRbcGczjuMoRm6dXaLDEhW1u34gKenbeYqAix21mdUKJyuyu5F1rzYGVxyL6tmgBUAEPrEz92mBXjByMRiJdba9wpnN37RLLAXa')
+    console.log('XPRIV')
     console.log(xpriv)
-    console.log('xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U')
+    console.log('xprv9s21ZrQH143K48vGoLGRPxgo2JNkJ3J3fqkirQC2zVdk5Dgd5w14S7fRDyHH4dWNHUgkvsvNDCkvAwcSHNAQwhwgNMgZhLtQC63zxwhQmRv')
 
 }
 
-generateMasterKey('fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542')
+generateMasterKey('3ddd5602285899a946114506157c7997e5444528f3003f6134712147db19b678')
--- utils/general.js
+++ utils/general.js
@@ -1,6 +1,21 @@
+// Function to convert a JS Buffer object to a binary string (example: '10011')
 export function convertBufferToBinaryString(buf) {
     const hex2bin = (data) => data.split('').map(i =>
         parseInt(i, 16).toString(2).padStart(4, '0')).join('');
     let hex = buf.toString('hex')
     return hex2bin(hex)
+}
+// Function to find the parity
+export function findParity(x) {
+    let y = x ^ (x >> 1);
+    y = y ^ (y >> 2);
+    y = y ^ (y >> 4);
+    y = y ^ (y >> 8);
+    y = y ^ (y >> 16);
+ 
+    // Rightmost bit of y holds the parity value
+    // if (y&1) is 1 then parity is odd else even
+    if (y & 1)
+        return 1;
+    return 0;
 }
